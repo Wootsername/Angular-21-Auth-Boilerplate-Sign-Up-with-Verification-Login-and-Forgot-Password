eea3e8ee1ccab2ba32e4d9d412b83ab1187bddf4
Alexx2378 <gaviolakarl.martin@gmail.com>
2026-05-18 18:40:23 +0800

    debug: add query parameter logging to ResetPasswordComponent ngOnInit

--- src/app/account/reset-password.component.ts
+++ src/app/account/reset-password.component.ts
@@ -30,7 +30,13 @@ export class ResetPasswordComponent implements OnInit {
 			validator: MustMatch('password', 'confirmPassword')
 		});
 
-		const token = this.route.snapshot.queryParams['token'];
+		console.log('QueryParams Snapshot:', this.route.snapshot.queryParams);
+		this.route.queryParams.subscribe(params => {
+			console.log('QueryParams Observable:', params);
+		});
+
+		const token = this.route.snapshot.queryParams['token'] || this.route.snapshot.queryParamMap.get('token');
+		console.log('Extracted token:', token);
 
 		this.accountService.validateResetToken(token)
 			.pipe(first())
